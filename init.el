5224c3d47739808cb9d457373b3804f89d039afd
Martin Leggewie <trommelmeister@gmx.de>
2021-04-24 23:45:19 +0200

    add one and only init.el, change gitignore

---
+++ init.el
@@ -0,0 +1,3 @@
+(org-babel-load-file "~/.emacs.d/emacs-user-configuration.org")
+(org-babel-load-file "~/.emacs.d/orgmode-common-configuration.org")
+(org-babel-load-file "~/.emacs.d/orgmode-user-configuration.org")
